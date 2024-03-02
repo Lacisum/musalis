325a609659ea4d9798b2f6fd78a5248b7e32a2a0
Lacisum <150604772+Lacisum@users.noreply.github.com>
2024-03-02 22:21:41 +0100

    markovModel: add constructor which builds 2D transition model ( => 1st order markov model)

---
+++ markov/markovModel.py
@@ -0,0 +1,8 @@
+class MarkovModel():
+
+	def __init__(self, states: tuple):
+		self.transition_matrix = dict()
+		for row in states:
+			self.transition_matrix[row] = dict()
+			for column in states:
+				self.transition_matrix[row][column] = 0.
