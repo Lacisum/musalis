856fa3c1a5f0783d56f59484e91a77ca3a3ff075
Lacisum <150604772+Lacisum@users.noreply.github.com>
2024-03-02 22:23:40 +0100

    MarkovModel: add next()

--- markov/markovModel.py
+++ markov/markovModel.py
@@ -1,3 +1,5 @@
+import random
+
 class MarkovModel():
 
 	def __init__(self, states: tuple):
@@ -7,3 +9,23 @@ class MarkovModel():
 			self.transition_matrix[row] = dict()
 			for column in states:
 				self.transition_matrix[row][column] = 0.
+
+	def next(self, prev_states: list):
+		'''
+		Returns a next state considering the previous states and the transition matrix.
+
+		Parameters:
+			- pre_states: the previous states
+		
+		Return:
+			the next state
+		'''
+		if len(prev_states) != 1:
+			raise Exception('There must be exactly 1 previous state')
+		if not all(prev in self.states for prev in prev_states):
+			raise Exception('At least one previous state is actually not a state')
+		tmp = self.transition_matrix
+		for prev in prev_states:
+			tmp = tmp[prev]
+		probabilities = (tmp[state] for state in self.states)
+		return random.choices(self.states, weights=probabilities)[0]
