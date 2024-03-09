2911334f8ebc9ec064aa07ab796e0615ed3d4ab5
Lacisum <150604772+Lacisum@users.noreply.github.com>
2024-03-09 23:35:47 +0100

    Graph: add add_node() code

--- musalis/markov/graph/graph.py
+++ musalis/markov/graph/graph.py
@@ -1,6 +1,19 @@
+from typing import Hashable
+
+from musalis.markov.graph.node import Node
+
+
+
 class Graph():
 
 
     def __init__(self):
         self.nodes = dict()
-    
+
+
+    def add_node(self, node_label):
+        if not isinstance(node_label, Hashable):
+            raise Exception("The node's label must be hashable")
+        if node_label in self.nodes.keys():
+            return
+        self.nodes[node_label] = Node(node_label)
