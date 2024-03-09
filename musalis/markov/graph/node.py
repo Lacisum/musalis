541e827c70159d54c13c99a0633c6d454611f72d
Lacisum <150604772+Lacisum@users.noreply.github.com>
2024-03-09 22:09:41 +0100

    Node: constructor raises exception if label is not hashable

--- musalis/markov/graph/node.py
+++ musalis/markov/graph/node.py
@@ -1,7 +1,13 @@
+from typing import Hashable
+
 class Node():
 
     def __init__(self, label):
-        pass
+        if not isinstance(label, Hashable):
+            raise Exception('The label must be hashable')
+        self.label = label
+        self.successors = []    # represent outgoing edges
+        self.weights = []       # represent outgoing edges' weights
 
     def add_successor(self, successor: 'Node', weight=1):
         pass
