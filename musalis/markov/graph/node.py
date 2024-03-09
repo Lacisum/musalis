343162dd910f41aba1db0f71b1db26fc23ce100b
Lacisum <150604772+Lacisum@users.noreply.github.com>
2024-03-09 21:35:51 +0100

    graph: add class Node

---
+++ musalis/markov/graph/node.py
@@ -0,0 +1,10 @@
+class Node():
+
+    def __init__(self, label):
+        pass
+
+    def add_successor(self, successor: 'Node', weight=1):
+        pass
+
+    def set_weight(self, index: int):
+        pass
