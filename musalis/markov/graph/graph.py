8436fc17feb38823a27aeca16690bbd659b42605
Lacisum <150604772+Lacisum@users.noreply.github.com>
2024-03-09 23:57:04 +0100

    graph: add documentation

--- musalis/markov/graph/graph.py
+++ musalis/markov/graph/graph.py
@@ -5,13 +5,22 @@ from musalis.markov.graph.node import Node
 
 
 class Graph():
+    '''A class for graphs.'''
 
 
     def __init__(self):
-        self.nodes = dict()
+        '''Creates a graph.'''
+        self.nodes = dict()     # {node_label: node}
 
 
     def add_node(self, node_label):
+        '''
+        If the given node label is not worn by any of the graph's
+        nodes, then adds a new node with that label to the graph.
+
+        Otherwise, does nothing (i.e. does not replace or modify
+        the already existing node).
+        '''
         if not isinstance(node_label, Hashable):
             raise Exception("The node's label must be hashable")
         if node_label in self.nodes.keys():
--- musalis/markov/graph/node.py
+++ musalis/markov/graph/node.py
@@ -3,17 +3,28 @@ from typing import Hashable
 
 
 class Node():
+    '''A class for nodes.'''
 
 
     def __init__(self, label):
+        '''
+        Creates a node with the given label.
+
+        The label must be hashable. If it is not, an exception
+        is raised.
+        '''
         if not isinstance(label, Hashable):
             raise Exception('The label must be hashable')
         self.label = label
-        self.successors = []    # represent outgoing edges
-        self.weights = []       # represent outgoing edges' weights
+        self.successors = []    # List of nodes. Represent outgoing edges
+        self.weights = []       # List of numbers (floats or ints). Represent outgoing edges' weights
 
 
     def add_successor(self, successor: 'Node', weight=1):
+        '''
+        Creates an edge between this node and the given node.
+        The edge gets the given weight (default weight is 1).
+        '''
         if not isinstance(successor, Node):
             raise Exception('Successor must be a Node')
         self.successors.append(successor)
