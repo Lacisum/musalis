65e38bead4a3e029a0efee21a89194e6b6a581cc
Lacisum <150604772+Lacisum@users.noreply.github.com>
2024-03-09 22:48:12 +0100

    GraphTest: add a test for constructor

--- musalis/test/markov/graph/testGraph.py
+++ musalis/test/markov/graph/testGraph.py
@@ -7,3 +7,11 @@ class GraphTest(unittest.TestCase):
     '''
     A test case for the Graph class.
     '''
+
+
+    def setUp(self):
+        self.graph = Graph()
+
+
+    def test_nodes_are_set_correctly(self):
+        self.assertEqual(dict(), self.graph.nodes)
