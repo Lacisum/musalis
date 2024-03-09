31a6887a6fcc0b8613ead135d38dcbbd3505323d
Lacisum <150604772+Lacisum@users.noreply.github.com>
2024-03-09 23:34:25 +0100

    GraphTest: update tests & add new tests for constructor and add_node()

--- musalis/test/markov/graph/testGraph.py
+++ musalis/test/markov/graph/testGraph.py
@@ -1,6 +1,7 @@
 import unittest
 
 from musalis.markov.graph.graph import Graph
+from musalis.markov.graph.node import Node
 
 
 class GraphTest(unittest.TestCase):
@@ -10,8 +11,22 @@ class GraphTest(unittest.TestCase):
 
 
     def setUp(self):
-        self.graph = Graph()
+        self.graph1 = Graph()
+        self.graph2 = Graph()
+        self.graph2.add_node('A')
 
 
     def test_nodes_are_set_correctly(self):
-        self.assertEqual(dict(), self.graph.nodes)
+        self.assertEqual(dict(), self.graph1.nodes)
+
+    def test_non_hashable_nodes_are_forbidden(self):
+        with self.assertRaises(Exception):
+            self.graph2.add_node(['not', 'hashable'])
+    
+    def test_adding_nodes_does_update_nodes(self):
+        self.assertEqual({'A': Node('A')}, self.graph2.nodes)
+        self.graph2.add_node('B')
+        self.assertEqual({'A': Node('A'), 'B': Node('B')}, self.graph2.nodes)
+
+    def test_trying_to_add_existing_node_does_nothing(self):
+        pass # TODO
